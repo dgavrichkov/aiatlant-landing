92d296dba3a4586a9bc1a17c3fb3fd653876a1a9
dgavrichkov <dgavrichkov32@gmail.com>
2024-09-16 18:59:30 +0300

    internationalization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: "English",
+  ru: "Русский",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    sliderPrev: "Swipe backward",
+    sliderNext: "Swipe forward",
+  },
+  ru: {
+    sliderPrev: "Прокрутить назад",
+    sliderNext: "Прокрутить вперед",
+  },
+} as const;
